9d7a1e2743307b1dc5a52ae7d9227f9da2e4e91b
helazak <hela.zakow@gmail.com>
2022-11-27 10:34:18 +0100

    adding projects

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from datetime import datetime
-from flask import Flask, render_template
+from flask import (
+    Flask,
+    redirect,
+    render_template, 
+    request,
+    url_for)
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -17,9 +22,49 @@ class Project(db.Model):
     finished = db.Column(db.Boolean)
 
     def __repr__(self):
-        return f'<Project {self.title}>'
+        return f'*Project {self.title}*'
 
 
 @app.route("/")
-def hello_world():
-    return render_template("index.html")
+def home():
+    projects = Project.query.all()
+
+    return render_template(
+        "index.html",
+        projects_list=projects,
+    )
+
+# żądanie wyświetl mi stronę główną - request HTTP GET /
+# żądanie wyświetl mi szczegóły zadania - request HTTP GET /task/{id}
+# żądanie wyświetl mi stronę z wszystkimi zadaniami - request HTTP GET /tasks
+# żądanie utwórz nowe zadanie - request HTTP POST /task + dane
+
+@app.route("/projects", methods=["POST"])
+def add_projects():
+    title = request.form.get("title")
+    category = request.form.get("category")
+    link = request.form.get("link")
+
+    new_project = Project(
+        title = title,
+        category = category,
+        link = link,
+    )
+
+#    new_project = Project(
+#    title = request.form.get("title")
+#    category = request.form.get("category")
+#    link = request.form.get("link")
+#    )
+    
+# dodanie projektu do bazy danych
+    db.session.add(new_project)
+    db.session.commit()
+    db.session.close()
+
+    return redirect(url_for('home'))
+#    projects = Project.query.all()
+#    return render_template(
+#        "index.html",
+#        projects_list=projects,
+#    )
